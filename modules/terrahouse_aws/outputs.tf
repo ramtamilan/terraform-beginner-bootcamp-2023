a6f75ba8402b12d21457717e28920449d3cd3bec
Ramaswamy Arunachalam <ramtamilan@yahoo.co.in>
2023-10-03 08:40:33 +0000

    #29 create cdn and serve s3 bucket from cdn

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -3,5 +3,5 @@ output "bucket_name" {
 }
 
 output "website_endpoint" {
-  value = aws_s3_bucket_website_configuration.example.website_endpoint
+  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
 }
